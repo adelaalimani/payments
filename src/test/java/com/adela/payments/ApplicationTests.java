6ddeace33ebbc033c2ba46c9c4012f1629e64cd9
Adela Alimani <adelaalimani1@gmail.com>
2026-08-24 17:28:54 +0200

    testing ci/cd

--- src/test/java/com/adela/payments/ApplicationTests.java
+++ src/test/java/com/adela/payments/ApplicationTests.java
@@ -1,10 +1,11 @@
 package com.adela.payments;
 
+import com.adela.payments.integration.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class ApplicationTests  extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
